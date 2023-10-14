d45b298360ef47195b9f432eb5ebf2642f9f2866
Nick Z <2420177+nickzelei@users.noreply.github.com>
2023-10-13 18:41:32 -0700

    adds env vars for pg15

--- envvars.zsh
+++ envvars.zsh
@@ -37,3 +37,9 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 # krew
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+# postgresql
+export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/postgresql@15/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/postgresql@15/include"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/postgresql@15/lib/pkgconfig"
