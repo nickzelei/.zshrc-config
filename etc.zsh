af0a4d0dc9e171e3fb70c0e732991f5474600317
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-02-05 11:02:15 -0800

    sets up pyenv, disables nucleus env vars

--- etc.zsh
+++ etc.zsh
@@ -2,5 +2,10 @@
 # autojump
 [ -f $HOMEBREW_PREFIX/etc/profile.d/autojump.sh ] && . $HOMEBREW_PREFIX/etc/profile.d/autojump.sh
 
+# gcp
 source $HOMEBREW_PREFIX/share/google-cloud-sdk/path.zsh.inc
 source $HOMEBREW_PREFIX/share/google-cloud-sdk/completion.zsh.inc
+
+# pyenv
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
