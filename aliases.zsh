74d957f93be17caa9c0b3c179c6d8a37a3a512d7
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-02-05 11:05:32 -0800

    more cleanup, commenting out unused stuff but may want later

--- aliases.zsh
+++ aliases.zsh
@@ -20,30 +20,16 @@ alias k="kubectl"
 alias avstage="av exec stage-from-nick --"
 alias avprod="av exec prod-from-nick --"
 alias avshared="av exec shared-from-nick --"
-alias apilocal="make build && avstage ./bin/nucleus-api --kube-config-path /Users/nick/.kube/config-local"
-get_kube_dash_token() {
-  kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}')
-}
-get_kaili_token() {
-    kubectl get secret -n istio-system $(kubectl get sa kiali-service-account -n istio-system -o "jsonpath={.secrets[0].name}") -o jsonpath={.data.token} | base64 -d
-}
-# switch_kube_ctx() {
-#     kubectl config use-context $1
-# }
-alias kubedashtoken='get_kube_dash_token'
-alias kailitoken='get_kaili_token'
-alias operatorlogs="kubectl logs deployment/nucleus-operator-controller-manager manager -n nucleus-operator-system"
-alias operatorlogs2="kubectl logs deployment/nucleus-operator-controller-manager manager -n nucleus"
-alias apilogs="kubectl logs -n nucleus-api deployment/nucleus-api user-container-grpc"
-alias mgmtlogs="kubectl logs deployment/mgmt-api user-container-grpc -n nucleus"
+
+# alias mgmtlogs="kubectl logs deployment/mgmt-api user-container-grpc -n nucleus"
 alias npods="k get pods -nnucleus"
 alias gmt="go mod tidy"
-alias customersvcs='kubectl get serviceaccount -l "api.usenucleus.cloud/service=customer-service" --all-namespaces'
-alias nat='cat ~/.nucleus/auth.yaml | yq ".accessToken"'
+# alias customersvcs='kubectl get serviceaccount -l "api.usenucleus.cloud/service=customer-service" --all-namespaces'
+# alias nat='cat ~/.nucleus/auth.yaml | yq ".accessToken"'
 alias kctx='kubectx'
 alias kctxstage='kubectx nucleus-stage-1'
 alias kctxprod='kubectx nucleus-prod-1'
-alias kctxacme='kubectx arn:aws:eks:us-west-2:911593346794:cluster/acme2'
+# alias kctxacme='kubectx arn:aws:eks:us-west-2:911593346794:cluster/acme2'
 alias kctxdev='kubectx kind-nuc-dev'
 
 # tilt
@@ -71,18 +57,19 @@ docker_rmi() {
 }
 # alias stage_tunnel='aws-vault exec stage-from-nick -- aws ssm start-session --region us-west-2 --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters host="nucleus-stage-nucleus.cluster-cffkzijt5nto.us-west-2.rds.amazonaws.com",portNumber="5432",localPortNumber="5433" --target $1'
 
-dev_role() {
-    echo "$NUCLEUS_DEV_ROLE_ARN" | pbcopy
-}
+# dev_role() {
+#     echo "$NUCLEUS_DEV_ROLE_ARN" | pbcopy
+# }
 
-setup_dev_cluster() {
-    aws-vault exec dev-nick-from-nick -- aws eks update-kubeconfig --region us-west-2 --name $1
-}
+# setup_dev_cluster() {
+#     aws-vault exec dev-nick-from-nick -- aws eks update-kubeconfig --region us-west-2 --name $1
+# }
 
 # git
 alias nzm="git fetch && git checkout master && git reset --hard origin/master"
 alias nzmm="git fetch && git checkout main && git reset --hard origin/main"
 alias gmcsg="gcmsg" # I keep typoing this
+alias gcsmg="gcmsg" # I keep typoing this too
 
-# Nucleus/Neosync
+# Neosync
 alias n="neosync"
