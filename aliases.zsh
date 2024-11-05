2bd18e5e7422cd063db4a28a7bfa8ff1e789b67f
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-11-05 15:39:03 -0800

    adds tfclean alias

--- aliases.zsh
+++ aliases.zsh
@@ -5,6 +5,8 @@ alias tg="terragrunt"
 alias tgp="terragrunt plan"
 alias tga="terragrunt apply"
 alias tgf="terragrunt hclfmt"
+alias tfclean="rm -rf .terraform .terraform.lock.hcl backend.tf providers.tf"
+
 
 function kubectl() {
     if ! type __start_kubectl >/dev/null 2>&1; then
