97708bc26cdcbf5bbc58e48f3bd234907eb46ed9
Nick Z <2420177+nickzelei@users.noreply.github.com>
2025-12-17 10:00:34 -0800

    adds openssl and mise to brewfile

--- etc.zsh
+++ etc.zsh
@@ -5,3 +5,7 @@
 # gcp
 # source $HOMEBREW_PREFIX/share/google-cloud-sdk/path.zsh.inc
 # source $HOMEBREW_PREFIX/share/google-cloud-sdk/completion.zsh.inc
+
+if command -v mise >/dev/null 2>&1; then
+  eval "$(mise activate zsh)"
+fi
