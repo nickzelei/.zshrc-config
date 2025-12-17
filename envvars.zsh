6d12a0eb10b81e66c9214f237aacc4b9caa32d9c
Nick Z <2420177+nickzelei@users.noreply.github.com>
2025-12-17 09:57:54 -0800

    removes pyenv and other unused env vars

--- envvars.zsh
+++ envvars.zsh
@@ -1,12 +1,6 @@
 # Go
-export GOPRIVATE=github.com/nucleuscloud/*
+# export GOPRIVATE=github.com/nucleuscloud/*
 export PATH="$PATH:$HOME/go/bin"
-
-# Neosync
-export PATH="$PATH:$HOME/code/nucleus/neosync/cli/bin"
-
-# kctxpatch
-export PATH="$PATH:$HOME/code/personal/kctxpatch/bin"
 
 # AWS
 export AWS_DEFAULT_REGION=us-west-2
@@ -15,24 +9,8 @@ export AWS_PAGER=""
 # openssl
 export PATH="$HOMEBREW_PREFIX/opt/openssl@3/bin:$PATH"
 
-#volta
-export VOLTA_HOME="$HOME/.volta"
-export PATH="$VOLTA_HOME/bin:$PATH"
-
 # rust
 export PATH="$HOME/.cargo/bin:$PATH"
-
-# krew
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
-
-# postgresql
-export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"
-export LDFLAGS="-L/opt/homebrew/opt/postgresql@15/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/postgresql@15/include"
-export PKG_CONFIG_PATH="/opt/homebrew/opt/postgresql@15/lib/pkgconfig"
-
-# pyenv
-export PYENV_ROOT="$HOME/.pyenv"
 
 export LESS="-R" # adding -X will prevent the pager from clearing
 
--- etc.zsh
+++ etc.zsh
@@ -5,11 +5,3 @@
 # gcp
 # source $HOMEBREW_PREFIX/share/google-cloud-sdk/path.zsh.inc
 # source $HOMEBREW_PREFIX/share/google-cloud-sdk/completion.zsh.inc
-
-# pyenv
-pyenv() {
-  unset -f pyenv
-  [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(command pyenv init -)"
-  pyenv "$@"
-}
