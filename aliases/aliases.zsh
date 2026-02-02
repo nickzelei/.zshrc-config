abf71af51789f5c666fabdb032693cf4ba44773c
Nick Z <2420177+nickzelei@users.noreply.github.com>
2026-02-01 18:34:28 -0800

    cleans up aliases

--- aliases/aliases.zsh
+++ aliases/aliases.zsh
@@ -23,32 +23,8 @@ alias k="kubectl"
 alias gmt="go mod tidy"
 
 alias kctx='kubectx'
-alias kctxstage='kubectx arn:aws:eks:us-west-2:997306413652:cluster/neosync-stage-2'
-alias kctxprod='kubectx nucleus-prod-1'
-alias kctxeuprod='kubectx neosync-prod-1-eu'
 
 alias brewup='brew update && brew upgrade && brew autoremove && brew cleanup'
-
-get_stage_tunnel() {
-    INSTANCE_ID=$(aws-vault exec stage -- aws ec2 describe-instances --filters "Name=tag:Name,Values=ssm-bastion-lt-stage" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text)
-    echo "Connecting to Jump Box: $INSTANCE_ID"
-    aws-vault exec stage -- aws ssm start-session --region us-west-2 --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters host="nucleus-stage-neosync.cffkzijt5nto.us-west-2.rds.amazonaws.com",portNumber="5432",localPortNumber="5443" --target $INSTANCE_ID
-}
-alias stage_tunnel='get_stage_tunnel'
-
-get_prod_tunnel() {
-    INSTANCE_ID=$(aws-vault exec prod -- aws ec2 describe-instances --filters "Name=tag:Name,Values=ssm-bastion-lt-prod" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text)
-    echo "Connecting to Jump Box: $INSTANCE_ID"
-    aws-vault exec prod -- aws ssm start-session --region us-west-2 --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters host="nucleus-prod-nucleus-one.cxmhfqug8ymr.us-west-2.rds.amazonaws.com",portNumber="5432",localPortNumber="5444" --target $INSTANCE_ID
-}
-alias prod_tunnel='get_prod_tunnel'
-
-# get_prod_tunnel_redis() {
-#     INSTANCE_ID=$(aws-vault exec prod -- aws ec2 describe-instances --filters "Name=tag:Name,Values=ssm-bastion-lt-prod" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text)
-#     echo "Connecting to Jump Box: $INSTANCE_ID"
-#     aws-vault exec prod -- aws ssm start-session --region us-west-2 --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters host="master.neosync-redis.jphk2x.usw2.cache.amazonaws.com",portNumber="6379",localPortNumber="6378" --target $INSTANCE_ID
-# }
-# alias prod_tunnel_redis='get_prod_tunnel_redis'
 
 docker_rmi() {
     docker rmi -f $(docker images -aq)
@@ -57,11 +33,7 @@ docker_rmi() {
 # git
 alias nzm="git fetch && git checkout master && git reset --hard origin/master"
 alias nzmm="git fetch && git checkout main && git reset --hard origin/main"
-alias devn="git fetch && git checkout develop/v0.5 && git reset --hard origin/develop/v0.5"
 alias gmcsg="gcmsg" # I keep typoing this
 alias gcsmg="gcmsg" # I keep typoing this too
-
-# Neosync
-alias n="neosync"
 
 source ~/.zshrc-config/aliases/directories.zsh
